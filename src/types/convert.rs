b388adf59ffc05adfa27b8a82c2189945c8f594c
George Makdesi <george.makdesi@icloud.com>
2026-01-11 18:39:54 +0100

    Introduce the convert mod.
    This change introduces the convert module that introduces traits equivalent to Rust's `From` and `Into` traits.

--- src/types/convert.rs
+++ src/types/convert.rs
@@ -6,7 +6,7 @@ use crate::types::core::{
     TypeF32, TypeF64, TypeI8, TypeI16, TypeI32, TypeI64, TypeU8, TypeU16, TypeU32, TypeU64,
 };
 
-/// Conversion hint, that tells if conversion from `T` to `U` is can be done by bitwise-copying bytes of `T`.
+/// Conversion hint, that tells if conversion from `T` to `U` can be achieved by copying bitwise bytes of `T`.
 pub struct CopyConversion<T: ?Sized, U: ?Sized>(bool, PhantomData<(*mut T, *mut U)>);
 
 impl<T: ?Sized, U: ?Sized> CopyConversion<T, U> {
@@ -76,22 +76,18 @@ macro_rules! impl_copy_conversion_for {
 
         impl CopyConversion<$native, $protocol> {
             /// Checks if conversion by copying between the two primitive types is enabled.
-            pub const PRIMITIVE: Self =
-                unsafe {
-                    CopyConversion::enable_if(
-                        size_of::<Self>() <= 1 || cfg!(target_endian = "little"),
-                    )
-                };
+            pub const PRIMITIVE: Self = unsafe {
+                CopyConversion::enable_if(size_of::<Self>() <= 1 || cfg!(target_endian = "little"))
+            };
         }
 
         impl CopyConversion<$protocol, $native> {
             /// Checks if conversion by copying between the two primitive types is enabled.
-            pub const PRIMITIVE: Self =
-                unsafe {
-                    CopyConversion::enable_if(
-                        CopyConversion::<$native, $protocol>::PRIMITIVE.is_enabled(),
-                    )
-                };
+            pub const PRIMITIVE: Self = unsafe {
+                CopyConversion::enable_if(
+                    CopyConversion::<$native, $protocol>::PRIMITIVE.is_enabled(),
+                )
+            };
         }
     };
 }
@@ -114,30 +110,30 @@ impl_copy_conversion_for! { native = f32, protocol = TypeF32 }
 impl_copy_conversion_for! { native = f64, protocol = TypeF64 }
 
 /// A type which is convertible from a protocol type.
-pub trait FromProtocolType<W>: Sized {
+pub trait FromProtocolType<P>: Sized {
     /// Checks if conversion by bitwise-copying is enabled.
-    const COPY_CONVERSION: CopyConversion<W, Self> = CopyConversion::disable();
+    const COPY_CONVERSION: CopyConversion<P, Self> = CopyConversion::disable();
 
     /// Converts the given `protocol_type` to this type.
-    fn from_protocol_type(protocol_type: W) -> Self;
+    fn from_protocol_type(protocol_type: P) -> Self;
 }
 
 /// A type which is convertible from a reference to a protocol type.
-pub trait FromProtocolTypeRef<W>: FromProtocolType<W> {
+pub trait FromProtocolTypeRef<P>: FromProtocolType<P> {
     /// Converts the given `protocol_type` reference to this type.
-    fn from_protocol_type_ref(protocol_type: &W) -> Self;
+    fn from_protocol_type_ref(protocol_type: &P) -> Self;
 }
 
-/// A convertible type from option of protocol type protocol type.
-pub trait FromOptionProtocolType<W>: Sized {
+/// A convertible type from option of protocol type.
+pub trait FromOptionProtocolType<P>: Sized {
     /// Converts the given `protocol_type` to an option of this type.
-    fn from_option_protocol_type(protocol_type: W) -> Option<Self>;
+    fn from_option_protocol_type(protocol_type: P) -> Option<Self>;
 }
 
 /// A convertible type from a reference to an option of protocol type.
-pub trait FromOptionProtocolTypeRef<W>: FromOptionProtocolType<W> {
+pub trait FromOptionProtocolTypeRef<P>: FromOptionProtocolType<P> {
     /// Converts the given `protocol_type` reference to an option of this type.
-    fn from_option_protocol_type_ref(protocol_type: &W) -> Option<Self>;
+    fn from_option_protocol_type_ref(protocol_type: &P) -> Option<Self>;
 }
 
 macro_rules! impl_from_protocol_type_for {
@@ -180,8 +176,8 @@ impl_from_protocol_type_for! { native = u64, protocol = TypeU64 }
 impl_from_protocol_type_for! { native = f32, protocol = TypeF32 }
 impl_from_protocol_type_for! { native = f64, protocol = TypeF64 }
 
-impl<T: FromProtocolType<W>, W, const N: usize> FromProtocolType<[W; N]> for [T; N] {
-    fn from_protocol_type(protocol_type: [W; N]) -> Self {
+impl<T: FromProtocolType<P>, P, const N: usize> FromProtocolType<[P; N]> for [T; N] {
+    fn from_protocol_type(protocol_type: [P; N]) -> Self {
         let mut result = MaybeUninit::<[T; N]>::uninit();
         if T::COPY_CONVERSION.is_enabled() {
             // Safety: `T` is convertible by copying bitwise its bytes.
@@ -204,8 +200,8 @@ impl<T: FromProtocolType<W>, W, const N: usize> FromProtocolType<[W; N]> for [T;
     }
 }
 
-impl<T: FromProtocolTypeRef<W>, W, const N: usize> FromProtocolTypeRef<[W; N]> for [T; N] {
-    fn from_protocol_type_ref(protocol_type: &[W; N]) -> Self {
+impl<T: FromProtocolTypeRef<P>, P, const N: usize> FromProtocolTypeRef<[P; N]> for [T; N] {
+    fn from_protocol_type_ref(protocol_type: &[P; N]) -> Self {
         let mut result = MaybeUninit::<[T; N]>::uninit();
         if T::COPY_CONVERSION.is_enabled() {
             // Safety: `T` is convertible by copying bitwise its bytes.
@@ -227,26 +223,26 @@ impl<T: FromProtocolTypeRef<W>, W, const N: usize> FromProtocolTypeRef<[W; N]> f
     }
 }
 
-impl<T: FromProtocolType<W>, W> FromProtocolType<W> for Box<T> {
-    fn from_protocol_type(protocol_type: W) -> Self {
+impl<T: FromProtocolType<P>, P> FromProtocolType<P> for Box<T> {
+    fn from_protocol_type(protocol_type: P) -> Self {
         Box::new(T::from_protocol_type(protocol_type))
     }
 }
 
-impl<T: FromProtocolTypeRef<W>, W> FromProtocolTypeRef<W> for Box<T> {
-    fn from_protocol_type_ref(protocol_type: &W) -> Self {
+impl<T: FromProtocolTypeRef<P>, P> FromProtocolTypeRef<P> for Box<T> {
+    fn from_protocol_type_ref(protocol_type: &P) -> Self {
         Box::new(T::from_protocol_type_ref(protocol_type))
     }
 }
 
-impl<T: FromOptionProtocolType<W>, W> FromProtocolType<W> for Option<T> {
-    fn from_protocol_type(protocol_type: W) -> Self {
+impl<T: FromOptionProtocolType<P>, P> FromProtocolType<P> for Option<T> {
+    fn from_protocol_type(protocol_type: P) -> Self {
         T::from_option_protocol_type(protocol_type)
     }
 }
 
-impl<T: FromOptionProtocolTypeRef<W>, W> FromProtocolTypeRef<W> for Option<T> {
-    fn from_protocol_type_ref(protocol_type: &W) -> Self {
+impl<T: FromOptionProtocolTypeRef<P>, P> FromProtocolTypeRef<P> for Option<T> {
+    fn from_protocol_type_ref(protocol_type: &P) -> Self {
         T::from_option_protocol_type_ref(protocol_type)
     }
 }
@@ -288,6 +284,6 @@ impl_into_native_for! { native = u64, protocol = TypeU64 }
 impl_into_native_for! { native = f32, protocol = TypeF32 }
 impl_into_native_for! { native = f64, protocol = TypeF64 }
 
-impl<W: IntoNativeType, const N: usize> IntoNativeType for [W; N] {
-    type NativeType = [W::NativeType; N];
+impl<P: IntoNativeType, const N: usize> IntoNativeType for [P; N] {
+    type NativeType = [P::NativeType; N];
 }

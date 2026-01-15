60be5450deb75790430f16d78960d07bc9b566cf
George Makdesi <george.makdesi@icloud.com>
2026-01-15 21:47:31 +0100

    Update main mod.
    That was left out.

--- rt/src/next/mod.rs
+++ rt/src/next/mod.rs
@@ -1,8 +1,8 @@
 //! The next-generation implementation of "Interconnect" that relies on its own data-model.
 
 pub mod codec;
+pub mod concurrency;
 pub mod error;
 pub mod mem;
 pub mod transport;
 pub mod types;
-pub mod concurrency;

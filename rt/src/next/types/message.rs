c47c21bb01bc8bdbdaf8b04dc2ec48aace1234d1
George Makdesi <george.makdesi@icloud.com>
2026-01-15 20:58:45 +0100

    Introduce message mod.
    This change introduces the message module and adds the header type.
    
    The new header has simplified version  compared to the previous header, but adds more bytes, the added bytes are required  for padding anyway, because the layout-rules of the message.
    
    The header has rules for fields' values, but I am not concerned with them now.

---
+++ rt/src/next/types/message.rs
@@ -0,0 +1,80 @@
+use std::mem::MaybeUninit;
+
+use zerocopy::IntoBytes;
+
+use crate::next::codec::{decode::Decode, encode::Encode};
+use crate::next::error::ProtocolResult;
+use crate::next::types::core::{ProtocolType, TypeU64};
+use crate::next::types::limits::Unlimited;
+
+pub type MessageID = TypeU64;
+
+pub type MessageDirective = TypeU64;
+
+/// 16-bytes header of Interconnect's message.
+///
+/// Header consists of two fields, each is 8-bytes in size and alignment.
+///
+/// TODO: Both have embedded bit-flags
+#[derive(Debug, Clone, Copy, IntoBytes)]
+#[repr(C)]
+pub struct TypeMessageHeader {
+    /// 8-bytes identifier of the message.
+    ///
+    /// TODO: Not all bytes are useable.
+    pub id: MessageID,
+
+    /// 8-bytes directive of the message.
+    ///
+    /// TODO: Not all bytes are useable.
+    pub directive: MessageDirective,
+}
+
+impl TypeMessageHeader {
+    #[inline]
+    pub const fn new(id: MessageID, directive: MessageDirective) -> Self {
+        Self { id, directive }
+    }
+}
+
+unsafe impl ProtocolType for TypeMessageHeader {
+    type Type<'de> = Self;
+
+    #[inline]
+    fn write_zero_padding(_to: &mut MaybeUninit<Self>) {}
+}
+
+impl Unlimited for TypeMessageHeader {}
+
+unsafe impl<E: ?Sized> Encode<TypeMessageHeader, E> for TypeMessageHeader {
+    fn encode(
+        self,
+        _encoder: &mut E,
+        inline_value: &mut MaybeUninit<TypeMessageHeader>,
+        _limits: <TypeMessageHeader as super::limits::TypeLimits>::Limits,
+    ) -> ProtocolResult<()> {
+        inline_value.write(self);
+        Ok(())
+    }
+}
+
+unsafe impl<E: ?Sized> Encode<TypeMessageHeader, E> for &TypeMessageHeader {
+    fn encode(
+        self,
+        encoder: &mut E,
+        inline_value: &mut MaybeUninit<TypeMessageHeader>,
+        limits: <TypeMessageHeader as super::limits::TypeLimits>::Limits,
+    ) -> ProtocolResult<()> {
+        Encode::encode(*self, encoder, inline_value, limits)
+    }
+}
+
+unsafe impl<D: ?Sized> Decode<D> for TypeMessageHeader {
+    fn decode(
+        _value: crate::next::codec::reference::TypeRef<'_, Self>,
+        _decoder: &mut D,
+        _limits: Self::Limits,
+    ) -> ProtocolResult<()> {
+        Ok(())
+    }
+}
--- rt/src/next/types/mod.rs
+++ rt/src/next/types/mod.rs
@@ -1,3 +1,4 @@
 pub mod convert;
 pub mod core;
 pub mod limits;
+pub mod message;

12ed079c8faed94f63b399f9cb9e164aa7bd9d51
George Makdesi <george.makdesi@icloud.com>
2026-02-01 08:23:02 +0100

    Introduce executor traits.
    This change introduces the "Executor" trait and its associated traits for abstracting over the actual async-runtime and its gears, or lets say, its currently-relevant gears.
    
    The traits are unstable and expected to change and adapt with more adoption.

--- rt/src/next/coop/traits.rs
+++ rt/src/next/coop/traits.rs
@@ -1,8 +1,9 @@
 use core::future::Future;
+use core::time::Duration;
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-use crate::next::error::ProtocolResult;
+use crate::next::error::{ProtocolError, ProtocolResult};
 
 pub trait BytesSender {
     fn send_bytes(&mut self, source: &[u8]) -> impl Future<Output = ProtocolResult<()>> + Send;
@@ -40,3 +41,37 @@ where
 
 pub trait BytesTransport: BytesSender + BytesReceiver {}
 impl<T> BytesTransport for T where T: BytesSender + BytesReceiver {}
+
+pub trait TimeInstant: Copy {
+    fn now() -> Self;
+    fn duration_since(&self, earlier: Self) -> Duration;
+}
+
+pub trait Timer {
+    type TimeInstant: TimeInstant;
+
+    fn sleep(duration: Duration) -> impl Future<Output = ()> + Send;
+
+    fn timeout<F, T>(duration: Duration, future: F) -> impl Future<Output = Result<T, ProtocolError>> + Send
+    where F: Future<Output = T> + Send;
+
+    fn timeout_at<F, T>(deadline: Self::TimeInstant, future: F) -> impl Future<Output = Result<T, ProtocolError>> + Send
+    where F: Future<Output = T> + Send;
+}
+
+pub trait ControlHandle<T: Send> {
+    type Error;
+    
+    fn abort(&self);
+    fn result(self) -> impl Future<Output = Result<T, Self::Error>> + Send;
+}
+
+pub trait Executor {
+    type ControlHandle<T: Send + 'static>: ControlHandle<T>;
+    type Timer: Timer;
+
+    fn spawn<F>(&self, future: F) -> Self::ControlHandle<F::Output>
+    where
+        F: Future + Send + 'static,
+        F::Output: Send + 'static;
+}

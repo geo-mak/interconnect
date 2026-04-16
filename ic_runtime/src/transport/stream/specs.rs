43e324f272a3d03b1b2a7ed75c262c44d22217f8
George Makdesi <george.makdesi@icloud.com>
2026-04-16 02:29:29 +0200

    Fix docs.
    I have learned something new: docs can cause cargo test to fail!

--- ic_runtime/src/transport/stream/specs.rs
+++ ic_runtime/src/transport/stream/specs.rs
@@ -101,12 +101,12 @@ impl EncryptionState {
 /// Initiation starts by sending `specification-frame` that is 8-bytes in size and its bytes
 /// represent the following:
 ///
-///     [0..4]   Protocol signature and version
-///     [4]      ABI version
-///     [5]      Encryption flags:
+/// [0..4]   Protocol signature and version
+/// [4]      ABI version
+/// [5]      Encryption flags:
 ///            0x00 = Unencrypted
 ///            0x01 = Encrypted
-///     [6..8]   Reserved.
+/// [6..8]   Reserved.
 ///
 /// The client-side is the side that initiates the negotiation using the `initiate` function.
 ///
@@ -114,8 +114,8 @@ impl EncryptionState {
 /// specifications with its configurations.
 ///
 /// The server then sends back its response as single byte that represents two states:
-///     0x00 = rejected/abort
-///     0x01 = accepted
+/// 0x00 = rejected/abort
+/// 0x01 = accepted
 ///
 /// If the specifications have the encryption-flag set and server has confirmed, the server should
 /// expect a key-exchange session using `accept_key_exchange` function.

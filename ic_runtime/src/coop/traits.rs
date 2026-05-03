6732f464d3993c25c2f1d58be0e2fcbb70f3d857
George Makdesi <george.makdesi@icloud.com>
2026-05-03 17:45:22 +0200

    Update docs.

--- ic_runtime/src/coop/traits.rs
+++ ic_runtime/src/coop/traits.rs
@@ -31,7 +31,14 @@ pub trait Timer {
 pub trait ControlHandle<T: Send> {
     type Error;
 
+    /// Sends abort signal to scheduler.
+    ///
+    /// Abort might not have an immediate effect and
+    /// may take effect after the task yield control to the scheduler.
     fn abort(&self);
+
+    /// Returns the result of the task **after** it finishes
+    /// either normally or as a consequence of aborting.
     fn result(self) -> impl Future<Output = Result<T, Self::Error>> + Send;
 }
 
@@ -39,6 +46,9 @@ pub trait TaskServer {
     type ControlHandle<T: Send + 'static>: ControlHandle<T>;
     type Timer: Timer;
 
+    /// Creates a new task and **schedules** it immediately.
+    ///
+    /// The task might end up being running immediately.
     fn create<F>(&self, future: F) -> Self::ControlHandle<F::Output>
     where
         F: Future + Send + 'static,
